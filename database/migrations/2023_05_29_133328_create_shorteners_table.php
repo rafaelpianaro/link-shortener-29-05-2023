c6e5a5c81e136e570982e44f42925ebc2a639250
Rafael Pianaro <rafaelpianaro@gmail.com>
2023-05-29 11:12:33 -0300

    add: migration, route for shorteners, model, controller

--- database/migrations/2023_05_29_133328_create_shorteners_table.php
+++ database/migrations/2023_05_29_133328_create_shorteners_table.php
@@ -11,13 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('shorteners', function (Blueprint $table) {
+            $table->string('identifier',8);
+            $table->string('title',50);
+            $table->string('url',500);
+            $table->integer('access');
+            $table->ipAddress('ip');
+            $table->string('user_agent',500);
             $table->timestamps();
         });
     }
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('shorteners');
     }
 };
