3c4e0e898a1349b7d79bb8afe54eed9355a01946
Rafael Pianaro <rafaelpianaro@gmail.com>
2023-05-30 12:44:18 -0300

    add: route for web and ShortenerController web

---
+++ app/Http/Controllers/Web/ShortenerController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use App\Http\Controllers\Controller;
+use App\Models\AccessDetails;
+use App\Models\Shortener;
+use Illuminate\Http\Request;
+
+class ShortenerController extends Controller
+{
+    public function handle(Request $request, $identifier)
+    {
+        $shortener = Shortener::where('identifier', '=', $identifier)->first();
+        if (!$shortener) {
+            return redirect('/');
+        } else {
+            // $data = AccessDetails::where('shortener_identifier', $identifier)->first();
+            $data = new AccessDetails();
+            $data['ip'] = $request->ip();
+            $data['user_agent'] = $request->server('HTTP_USER_AGENT');
+            $data['shortener_identifier'] = $identifier;
+            $data->fill($data->toArray());
+		    $data->save();
+
+            Shortener::where('identifier', $identifier)->increment('access');
+
+            return redirect($shortener['url']);
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,12 @@
 <?php
 
+use App\Http\Controllers\Web\ShortenerController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/shortener/{id}', [ShortenerController::class, 'handle']);
+
+// Route::get('shortener/{shortener}','Web\ShortenerController@handle');
