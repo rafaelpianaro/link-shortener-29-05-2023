2e59c0273722b7418aa66fc165124dbece32cc1e
Rafael Pianaro <rafaelpianaro@gmail.com>
2023-05-30 12:41:02 -0300

    add: access_details table

---
+++ database/migrations/2023_05_30_120704_access_details.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('access_details', function (Blueprint $table) {
+            // $table->increments('id');
+            $table->ipAddress('ip')->nullable();
+            $table->string('user_agent',500)->nullable();
+            $table->string('shortener_identifier')->nullable();
+            $table->foreign('shortener_identifier')->references('identifier')->on('shorteners');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('access_details');
+    }
+};
